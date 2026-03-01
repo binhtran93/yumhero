113e4a9601f22e81327ab4c782103f7c6efd303a
binhtran <tdbinh93@gmail.com>
2026-03-01 08:44:06 +0700

    log request

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,13 +1,13 @@
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const headers = Object.fromEntries(event.request.headers.entries());
-	console.info(
-		JSON.stringify({
-			method: event.request.method,
-			url: `${event.url.pathname}${event.url.search}`,
-			headers
-		})
-	);
+	// const headers = Object.fromEntries(event.request.headers.entries());
+	// console.info(
+	// 	JSON.stringify({
+	// 		method: event.request.method,
+	// 		url: `${event.url.pathname}${event.url.search}`,
+	// 		headers
+	// 	})
+	// );
 	return resolve(event);
 };
